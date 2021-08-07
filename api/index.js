12467a89065b08e7bff40b24a6f77cec0c21c1c3
Julia Chen <juliazychen@gmail.com>
2021-08-07 12:58:02 -0400

    added environment variable support with .env

--- api/index.js
+++ api/index.js
@@ -1,11 +1,20 @@
-import express from "express"
+import express from 'express'
+import { MongoClient } from 'mongodb'
+import dotenv from 'dotenv'
+dotenv.config()
 
 const PORT = process.env.PORT || 8000
 
 const app = express()
 
-app.get("/test", (_req, res, _err) => res.json({ response: "Hello World!" }))
+app.get('/test', (_req, res, _err) => res.json({ response: 'Hello World!' }))
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
     console.log(`Server running on port ${PORT}`)
+    const client = new MongoClient(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true })
+    await client.connect();
+    const collection = await client.db('PersonalManagement').collection('Tasks')
+    const query = await collection.find({ status: false })
+    console.log(await query.toArray())
+    await client.close()
 })
