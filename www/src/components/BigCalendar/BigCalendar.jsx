129d7f1f638bb9ab95f20e5ac84b60efca937178
Harit Kapadia <haritkapadia@outlook.com>
2021-08-08 03:00:08 +0000

    Connected to API

--- www/src/components/BigCalendar/BigCalendar.jsx
+++ www/src/components/BigCalendar/BigCalendar.jsx
@@ -4,66 +4,17 @@ import { Views } from 'react-big-calendar'
 import 'react-big-calendar/lib/css/react-big-calendar.css'
 import { MainCalendar } from './styles.js'
 
-class Selectable extends React.Component {
-  constructor (...args) {
-    super(...args)
+const BigCalendar = ({ localizer, events, onSelectEvent, onSelectSlot }) => (
+  <MainCalendar
+    localizer={localizer}
+    events={events}
+    onSelectEvent={onSelectEvent}
+    onSelectSlot={onSelectSlot}
+    selectable
+    defaultView={Views.MONTH}
+    scrollToTime={new Date(1970, 1, 1, 6)}
+    defaultDate={new Date()}
+  />
+)
 
-    this.state = {
-      events: [
-        {
-          title: 'Big Meeting',
-          allDay: true,
-          start: new Date(2021, 6, 0),
-          end: new Date(2021, 6, 0)
-        },
-        {
-          title: 'Vacation',
-          start: new Date(2021, 6, 7),
-          end: new Date(2021, 6, 10)
-        },
-        {
-          title: 'Conference',
-          start: new Date(2021, 6, 20),
-          end: new Date(2021, 6, 23)
-        }
-      ]
-    }
-  }
-
-  handleSelect = ({ start, end }) => {
-    const title = window.prompt('New Event name')
-    if (title) {
-      this.setState({
-        events: [
-          ...this.state.events,
-          {
-            start,
-            end,
-            title
-          }
-        ]
-      })
-      console.log(title)
-    }
-  }
-
-  render () {
-    const { localizer } = this.props
-    return (
-      <>
-        <MainCalendar
-          selectable
-          localizer={localizer}
-          events={this.state.events}
-          defaultView={Views.MONTH}
-          scrollToTime={new Date(1970, 1, 1, 6)}
-          defaultDate={new Date()}
-          onSelectEvent={event => alert(event.title)}
-          onSelectSlot={this.handleSelect}
-        />
-      </>
-    )
-  }
-}
-
-export default Selectable
+export default BigCalendar
